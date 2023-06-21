033fa09e39407def6dfdffa80687efb183808309
Mario <michaelcmorgans1@gmail.com>
2023-06-21 17:44:21 -0500

    in progress..

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,10 +2,7 @@ fn main() {
     println!(
         "{}",
         print_phase("{I am the argument for print_pharse, a &str}é")
-
     );
-
-
 }
 
 fn print_phase(phrase: &str) -> usize {
@@ -14,15 +11,14 @@ fn print_phase(phrase: &str) -> usize {
 }
 
 fn gcd(mut a: u64, mut b: u64) -> u64 {
-    
-    while a!= 0{
-        if a<b{
+    while a != 0 {
+        if a < b {
             let c = a;
-            a=b
-            b=c
+            a = b;
+            b = c;
         }
-        a = a%b;
+        a = a % b;
     }
-    
+
     b
 }
