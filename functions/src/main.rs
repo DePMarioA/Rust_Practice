2cc9a4aad754275d140e906b435328dce4ef94be
Mario <michaelcmorgans1@gmail.com>
2023-06-23 13:22:02 -0500

    minor edits, readme_functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,6 +3,26 @@ fn main() {
         "{}",
         print_phase("{I am the argument for print_pharse, a &str}é")
     );
+
+    let answer: u64 = gcd(20, 5);
+    println!("{answer}");
+    let mut x = 0;
+    'loopA: loop {
+        let mut y = 0;
+        println!("loopA: {x}");
+        loop {
+            println!("{:<5}loop2", "");
+            if x > 1 {
+                println!("{:<5}Breaking from 'loopA", "");
+                break 'loopA;
+            }
+            if y == 1 {
+                break;
+            }
+            y += 1;
+        }
+        x += 1;
+    }
 }
 
 fn print_phase(phrase: &str) -> usize {
